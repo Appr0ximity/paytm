6cd0383aed567e45d22e2af211a40456e4af827d
Appr0ximity <hvshekharmurthy@gmail.com>
2024-07-17 14:15:51 +0530

    added signup route for users

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose")
 
 mongoose.connect('mongodb+srv://hvshekharmurthy:IB3uCeSUPKXxZ8ti@c1.ujnqw74.mongodb.net/paytm')
+
+const {Schema} = mongoose
+
+const userSchema = new Schema({
+    username: String,
+    firstName: String,
+    lastName: String,
+    password: String
+})
+
+export const User = mongoose.model("User", userSchema)
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,56 @@
+import { JWT_SECRET } from "../config"
+const jwt = require("jsonwebtoken")
 const express = require("express")
+const zod = require("zod")
+const {User} = require("../db")
 
 const app = express()
 
+const userSignup = zod.object({
+    username: zod.email({
+        message: "Invalid email"
+    }),
+    firstName: zod.string(),
+    lastName : zod.string(),
+    password: zod.string()
+})
+
 export const userRouter = express.Router()
+
+userRouter.post("/signup", async (req, res)=>{
+    const {username, firstname, lastname, password} = req.body
+    const credentials = {
+        username: username,
+        firstName: firstname,
+        lastname: lastname,
+        password: password
+    }
+    const credVerification = userSignup.safeParse(credentials)
+    if(!credVerification.success){
+        credVerification.error.issues
+        return res.status(411).json({
+            message:"Incorrect inputs"
+        })
+    }
+    else{
+        const existingUser = User.findOne({
+            username: credentials.username
+        })
+        if(existingUser){
+            return res.status(411).json({
+                message:"Existing User!!"
+            })
+        }
+        const user = await User.create(credentials)
+        const userID = user._id
+
+        const token = jwt.sign({
+            userID
+        }, JWT_SECRET)
+
+        return res.json({
+            message:"User created successfully",
+            token: token
+        })
+    }
+})
